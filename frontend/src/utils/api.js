bdeed170886d8f01753d7c23b31d14ee44c4689f
Pratik578 <pgadekar784@gmail.com>
2026-05-02 10:54:33 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,10 +1,12 @@
+//centralized API setup
+
 import axios from 'axios';
 import qs from 'qs';
 
 const api = axios.create({
-    baseURL: 'https://food-ordering-system-kmtj.onrender.com/api',
-    withCredentials: true,
-    paramsSerializer: params => qs.stringify(params, { arrayFormat: 'repeat' }),
-});
+    baseURL:'/api',
+    withCredentials:true,
+    paramsSerializer: params => qs.stringify(params, {arrayFormat:'repeat'}),
+})
 
 export default api;
